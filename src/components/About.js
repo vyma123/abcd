4b72a4f942f982e8ea7f400a19f383caafbade6c
Vyma <mavanvy2023@gmail.com>
2023-07-03 17:12:45 +0700

    changing background to white

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
-import aboutImg from '../images/about.jpeg';
-import Title from './Title';
+import aboutImg from "../images/about.jpeg";
+import Title from "./Title";
 
 const About = () => {
   return (
